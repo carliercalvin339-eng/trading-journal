fd10521e51459b22f9fd7d8b8ee5f2fa3ab2bc64
carliercalvin339-eng <carliercalvin339@gmail.com>
2026-08-18 00:10:57 +0200

    Add files via upload

---
+++ layout.js
@@ -0,0 +1,3 @@
+import './globals.css';
+export const metadata={title:'Trading Journal',description:'A focused trading journal'};
+export default function RootLayout({children}){return <html lang="fr"><body>{children}</body></html>}
